9b4c568877bda6db9db9c3c073fcc2f9fdf0ff5b
JCarlosDiaz <carlosdiazgongora@gmail.com>
2018-05-21 18:18:07 -0500

    Commit inicial

---
+++ scripts/custom.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    if($(window).width() > 991){
+        $('.tab-style-3 .nav-tabs li').each(function(e){
+            var position = $(this).position();
+            var left = position.left;
+            var a_href = $(this).find('a').attr('href');
+            var ul_to_move = $(a_href).find('ul');
+            ul_to_move.css('left', left-50);
+            $('#especiales ul').css('left', left-150);
+        });
+    }
+        
+});
