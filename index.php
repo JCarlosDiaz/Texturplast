848161f5c42ec9cdf481f7667fc0c2b6442ae22f
Dapper 02 <angelfcancun@gmail.com>
2018-10-04 15:34:43 -0500

    Links

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ nav-hidden - Open nav on icon click
                         <div class="col-md-12 social-container">
                             <a target="_blank" href="https://www.facebook.com/texturplast.mex/"><img src="images/texturplast/facew.svg" width="35px" alt=""></a>
                             <a target="_blank" href="https://www.facebook.com/texturplast.mex/"><img src="images/texturplast/instagramwhite.svg" width="35px" alt=""></a>
-                            <a target="_blank" href="https://www.facebook.com/texturplast.mex/"><img src="images/texturplast/linkw.svg" width="35px" alt=""></a>
+                            <a target="_blank" href="https://www.linkedin.com/company/33311358/admin/overview/"><img src="images/texturplast/linkw.svg" width="35px" alt=""></a>
                             
                         </div>
                     </div>
